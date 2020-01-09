9ecfcac29f5e655543ac3e971026e6b72ab284d8
marco <28888207+Marco3jp@users.noreply.github.com>
2020-01-10 04:34:47 +0900

    タスクを入力すろページを実装

--- pages/record/index.vue
+++ pages/record/index.vue
@@ -1,11 +1,114 @@
 <template>
+  <div>
+    <v-form @submit.prevent="postRecord">
+      <v-textarea label="メモなど" v-model="record.meta.note"></v-textarea>
+      <task-form v-for="(task, index) in record.tasks" :task="task" :key="index" :index="index"
+                 @decrease="decreaseTaskRatio" @increase="increaseTaskRatio"></task-form>
+      <v-btn outlined @click="pushNewTask" type="button">+</v-btn>
+      <task-form :task="unknownTask" :isUnknown="true"></task-form>
+      <v-btn outlined type="submit">記録を保存</v-btn>
+      <p class="red--text text--lighten-1" v-show="errorMessage !== ''">{{errorMessage}}</p>
+    </v-form>
+  </div>
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
+    import {TasksRecord} from "~/model/tasksRecord";
+    import TaskForm from "~/components/taskForm.vue";
+    import {Task} from "~/model/task";
+
+    interface recordData {
+        record: TasksRecord
+        unknownTask: Task,
+        decreaseTarget: number,
+        errorMessage: string,
+    }
 
     export default Vue.extend({
-        name: "record"
+        name: "record",
+        components: {TaskForm},
+        data(): recordData {
+            return {
+                record: {
+                    meta: {
+                        note: ""
+                    },
+                    tasks: [],
+                },
+                unknownTask: {
+                    ratio: 100,
+                    name: "不明"
+                },
+                decreaseTarget: 0,
+                errorMessage: "",
+            }
+        },
+        methods: {
+            decreaseTaskRatio(number: number) {
+                this.unknownTask.ratio += number;
+            },
+            increaseTaskRatio(index: number, number: number) {
+                if (this.unknownTask.ratio >= number) {
+                    // 不明領域から引くケース
+                    this.unknownTask.ratio -= number;
+                } else {
+                    number -= this.unknownTask.ratio;
+                    this.unknownTask.ratio = 0;
+
+                    // 他タスクから少しずつもらう場合
+                    while (number !== 0) {
+                        if (this.record.tasks[this.decreaseTarget].ratio > 0 && this.decreaseTarget !== index) {
+                            this.record.tasks[this.decreaseTarget].ratio--;
+                            number--;
+                        }
+
+                        if (this.decreaseTarget === this.record.tasks.length - 1) {
+                            this.decreaseTarget = 0;
+                        } else {
+                            this.decreaseTarget++;
+                        }
+                    }
+                }
+            },
+            pushNewTask() {
+                this.record.tasks.push({
+                    ratio: 0,
+                    name: ""
+                })
+            },
+            postRecord() {
+                if (this.validateForm()) {
+                    if (this.unknownTask.ratio > 0) {
+                        this.record.tasks.push(this.unknownTask);
+                    }
+                    this.$store.commit('record/unshift', JSON.parse(JSON.stringify(this.record)));
+                    console.log(this.$store.state.record.list);
+                    this.resetForm();
+                } else {
+                    this.errorMessage = "フォームの内容に不備がある可能性があります";
+                    setTimeout(() => {
+                        this.errorMessage = ""
+                    }, 10000);
+                }
+            },
+            resetForm() {
+                this.record.meta.note = "";
+                this.record.tasks.splice(0);
+                this.unknownTask.ratio = 100;
+                this.decreaseTarget = 0;
+            },
+            validateForm(): boolean {
+                return !(this.record.tasks.length === 0 || !this.validateRatio());
+            },
+            validateRatio() {
+                let ratio = this.unknownTask.ratio;
+                this.record.tasks.forEach(task => {
+                    ratio += task.ratio;
+                });
+                return ratio === 100;
+            }
+        },
     });
 </script>
 
